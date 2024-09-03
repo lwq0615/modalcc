34e07923c2ebe8cbabb920a0fa6c66913786b6ad
liweiqiang <liweiqiang@taqu.cn>
2024-09-03 14:08:11 +0800

    babel

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,7 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        targets: {
-          node: 'current',
-        },
+        modules: false
       },
     ],
     '@babel/preset-typescript',

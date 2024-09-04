b8d7868b327c79a068732011bb6dfaefe9f34a93
liweiqiang <liweiqiang@taqu.cn>
2024-09-04 09:59:29 +0800

    rm

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,27 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false
-      },
+const fs = require('fs');
+const path = require('path');
+const { rimrafSync } = require('rimraf')
+
+module.exports = function (api) {
+  api.cache(true);
+  const modulesMap = {
+    cjs: 'commonjs',
+    esm: false,
+  };
+  const distPath = path.resolve(__dirname, `./dist/${process.env.type}`);
+  if (fs.existsSync(distPath)) {
+    rimrafSync(distPath);
+  }
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: modulesMap[process.env.type],
+        },
+      ],
+      '@babel/preset-typescript',
     ],
-    '@babel/preset-typescript',
-  ],
-  ignore: [
-    "src/types"
-  ]
+    ignore: ['src/types'],
+  };
 };
